04ef8dc70feac575f78a65216f30effcf3bce2d1
mohamedshehab <mshehab841@gmail.com>
2024-03-19 03:44:04 +0200

    feat : more feature here image generator take first word in first name and make it defult image and cloudinary and make post and like and comment resolver chain

--- src/middleware/send-email.ts
+++ src/middleware/send-email.ts
@@ -1,5 +1,4 @@
 import SibApiV3Sdk from 'sib-api-v3-sdk'
-import dotenv from 'dotenv';dotenv.config()
 const defaultClient = SibApiV3Sdk.ApiClient.instance
 const apiKey = defaultClient.authentications['api-key']
 apiKey.apiKey = process.env.SIB_API
